9c17757ee54423b7775c2233b917e0fd64e3abda
s016 <hogehoge.hoge.113@gmail.com>
2022-10-06 23:07:56 +0900

    deviseにnameカラムの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "名前が空白でないこと" do
+    user = User.new(name: nil)
+    expect(user).to be_invalid  
+  end
+
+  it "名前は30文字以内であること" do
+    user = User.new(name: "a" * 31)
+    expect(user).to be_invalid
+  end
+
+  it "メールアドレスが空白でないこと" do
+    user = User.new(email: nil)
+    expect(user).to be_invalid
+  end
+
+  it "パスワードは6文字以上であること" do
+    user = User.new(password: "a" * 5)
+    expect(user).to be_invalid
+  end
 end
