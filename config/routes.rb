c58b9a36d90042d23edbe9bc6a41f801b77ef56c
s016 <hogehoge.hoge.113@gmail.com>
2022-10-07 21:28:29 +0900

    deviseのregistrations_controllerを有効にするように変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#main_page'
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'   
   }
 end
