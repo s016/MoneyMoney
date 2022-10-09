aeb7f8b82036d35df92a3b88cae546aaf56cad48
s016 <hogehoge.hoge.113@gmail.com>
2022-10-09 13:31:12 +0900

    本番環境用のdockerfileを追加

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+cd /MoneyMoney
+rm -f /myapp/tmp/pids/server.pid
+#bundle exec rails db:create RAILS_ENV=production
+#bundle exec rails db:migrate RAILS_ENV=production
+#bundle exec rails db:seed RAILS_ENV=production
+bundle exec rails assets:precompile RAILS_ENV=production
+exec "$@"
