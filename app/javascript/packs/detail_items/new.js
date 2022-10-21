62261536d5ecf23cbb7c6a581341819829278281
s016 <hogehoge.hoge.113@gmail.com>
2022-10-21 15:30:01 +0900

    app/javascript配下の不要ファイルの削除

--- app/javascript/packs/detail_items/new.js
+++ app/javascript/packs/detail_items/new.js
@@ -1,1 +1,13 @@
-require('detail_items/new')
+$(function () {
+  $("#detail_item_income_or_payment_1").click(function () {
+    $.get({
+      url: "/detail_items/select_incomes",
+    });
+  });
+
+  $("#detail_item_income_or_payment_2").click(function () {
+    $.get({
+      url: "/detail_items/select_payments",
+    });
+  });
+});
--- app/javascript/packs/income_and_payments/new.js
+++ app/javascript/packs/income_and_payments/new.js
@@ -1,1 +1,20 @@
-require('income_and_payments/new')
+$(function () {
+  $("#income_and_payment_item_id").change(function () {
+    $.get({
+      url: "/income_and_payments/select_item",
+      data: { item_id: $("#income_and_payment_item_id").has("option:selected").val() }
+    });
+  });
+
+  $("#income_and_payment_income_or_payment_1").click(function () {
+    $.get({
+      url: "/income_and_payments/select_incomes",
+    });
+  });
+
+  $("#income_and_payment_income_or_payment_2").click(function () {
+    $.get({
+      url: "/income_and_payments/select_payments",
+    });
+  });
+});
