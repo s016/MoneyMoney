9ad0dc085ae15ba8f7c2f7f2a814c2aa64c243f8
s016 <hogehoge.hoge.113@gmail.com>
2022-11-17 02:43:38 +0900

    収入と支払の登録を確定した日付以前で登録できないようにバリデーションを追加

--- app/models/income_and_payment.rb
+++ app/models/income_and_payment.rb
@@ -6,7 +6,8 @@ class IncomeAndPayment < ApplicationRecord
   validates :date, presence: true
   validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :income_or_payment, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 2 }
-  validate :invalid_date
+  validate :invalid_before_money_places_date
+  validate :invalid_before_confirm_date
   INCOMES = 1
   PAYMENTS = 2
   THREE_YEAR_TO_MONTH_MINUS_SAVED_MAONTH = 35
@@ -19,12 +20,22 @@ class IncomeAndPayment < ApplicationRecord
     income_and_payment_date = income_and_payment_params
   end
 
-  def invalid_date
-    if self.money_place.date && self.date.present?
+  def invalid_before_money_places_date
+    if self.money_place.date.present? && self.date.present?
       income_and_payment_date = self.date
       money_place_date = self.money_place.date
       if income_and_payment_date < money_place_date
         errors.add(:date, "お金の場所に登録した日付より前の日付は登録できません。")
+      end
+    end
+  end
+
+  def invalid_before_confirm_date
+    if self.user.actual_moneies.present? && self.date.present?
+      income_and_payment_date = self.date
+      confirm_date =  ActualMoney.where(user_id: self.user.id).present? ? self.user.actual_moneies.maximum(:date) : ActualMoney::NONE_CONFIRM_DATE
+      if income_and_payment_date <= confirm_date
+        errors.add(:date, "確定した日付より前の日付は登録できません。")
       end
     end
   end
