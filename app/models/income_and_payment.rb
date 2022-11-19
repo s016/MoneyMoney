e27fa58a89a05ac4a6f1ecb2e718fded00f4eadb
s016 <hogehoge.hoge.113@gmail.com>
2022-11-20 01:31:53 +0900

    お金の場所を確定した日付より前に登録できないようにバリデーションの設定を追加

--- app/models/income_and_payment.rb
+++ app/models/income_and_payment.rb
@@ -10,7 +10,7 @@ class IncomeAndPayment < ApplicationRecord
   validate :invalid_before_confirm_date
   INCOMES = 1
   PAYMENTS = 2
-  THREE_YEAR_TO_MONTH_MINUS_SAVED_MAONTH = 11
+  THREE_YEAR_TO_MONTH_MINUS_SAVED_MAONTH = 35
 
   def self.find_money_place_date(money_place_id)
     money_place_date = MoneyPlace.find_by(id: money_place_id).date
--- app/models/money_place.rb
+++ app/models/money_place.rb
@@ -5,4 +5,15 @@ class MoneyPlace < ApplicationRecord
   validates :name, presence: true
   validates :date, presence: true
   validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :invalid_before_confirm_date
+
+  def invalid_before_confirm_date
+    if self.user.money_places.present? && self.date.present?
+      money_place_date = self.date
+      confirm_date =  ActualMoney.where(user_id: self.user.id).present? ? self.user.actual_moneies.maximum(:date) : ActualMoney::NONE_CONFIRM_DATE
+      if money_place_date <= confirm_date
+        errors.add(:date, "確定した日付より前の日付は登録できません。")
+      end
+    end
+  end
 end
