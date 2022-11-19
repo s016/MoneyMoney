41fa757d1ca6e17544b697291989e6b3e9d2bf7c
s016 <hogehoge.hoge.113@gmail.com>
2022-11-20 01:20:42 +0900

    確定処理した場合の計算と表示が変わるように変更

--- app/models/result.rb
+++ app/models/result.rb
@@ -3,9 +3,13 @@ class Result < ApplicationRecord
   #計算の対象期間
   def self.result_date(current_user)
     #お金の場所の登録された一番古い日付を取得、確定処理を実装したら、確定処理から3年間に変更する。
-    start_day = current_user.money_places.minimum(:date)
+    if current_user.actual_moneies.present?
+      start_day = current_user.actual_moneies.maximum(:date)
+    else
+      start_day = current_user.money_places.minimum(:date)
+    end
     #3年後の月末まで
-    end_day = start_day.since(1.year).end_of_month
+    end_day = start_day.since(3.year).end_of_month
     (start_day..end_day).select { |date| date }
   end
 
@@ -34,11 +38,15 @@ class Result < ApplicationRecord
       self.result_date(current_user).each do |result_date|
         result[result_date.to_s] = {}
         money_places.each do |money_place|
-          start_day = money_place.date
-          end_day = result_date
+          start_day = current_user.actual_moneies.present? ? current_user.actual_moneies.maximum(:date) : money_place.date
+          end_day = start_day.since(3.year).end_of_month
           incomes = IncomeAndPayment.where(money_place_id: money_place.id, date: start_day..end_day, income_or_payment: IncomeAndPayment::INCOMES).sum(:amount)
           payments = IncomeAndPayment.where(money_place_id: money_place.id, date: start_day..end_day, income_or_payment: IncomeAndPayment::PAYMENTS).sum(:amount)
-          result[result_date.to_s][money_place.name] = money_place.amount + incomes - payments
+          if money_place.actual_moneies.present?
+            result[result_date.to_s][money_place.name] = money_place.actual_moneies.find_by(date: money_place.actual_moneies.maximum(:date)).amount + incomes - payments
+          else
+            result[result_date.to_s][money_place.name] = money_place.amount + incomes - payments
+          end
         end
       end
     end
